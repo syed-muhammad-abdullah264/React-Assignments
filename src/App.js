03ed70ca40278a2d103f2c90884b78ce10cdf954
Syed Muhammad Abdullah <s.abdullahkhan2008@gmail.com>
2025-11-26 08:00:30 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import "./global.css";
+import Header from "./components/header";
+import Form from "./components/form";
+import Counter from "./project-components/02-counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Header /> */}
+      <Form />
+      {/* <Filter /> */}
+      {/* <List /> */}
+      {/* <Counter /> */}
     </div>
   );
 }
---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import "./index.css";
+
+function Header() {
+  return (
+    <div className="header-wrapper center column">
+      <header>
+        <h1>Hello world!</h1>
+      </header>
+      <p>Hi! I am Abdullah</p>
+    </div>
+  );
+}
+
+export default Header;
