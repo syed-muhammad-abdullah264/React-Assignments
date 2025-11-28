3bcc46037e7226f8e9795796bf760e1c25cdb259
Syed Muhammad Abdullah <s.abdullahkhan2008@gmail.com>
2025-11-28 03:04:49 -0800

    some issue

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 // import "./App.css";
 import "./global.css";
-import Header from "./components/header";
 import Form from "./components/form";
-import Counter from "./project-components/02-counter";
-
 function App() {
   return (
     <div>
-      {/* <Header /> */}
       <Form />
-      {/* <Filter /> */}
-      {/* <List /> */}
-      {/* <Counter /> */}
     </div>
   );
 }
